96a36b232c30b1d831bd142b3a73e001df458584
xiaosong <xiaosong@baidu.com>
2023-03-20 10:52:43 +0800

    调整健康检查端口

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func main() {
 	var probeAddr string
 	flag.StringVar(&httpServerAddr, "http-server-bind-address", ":8080", "The address the http server binds to.")
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8081", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8082", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
